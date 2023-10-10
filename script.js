7931a976658e60ce6d6b47d68695bb1e85b446e7
erpiedra <pedrogris@correo.ugr.es>
2023-10-10 19:45:35 +0200

    Terminado

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const passwordBox = document.getElementById("password");
+const length = 20;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]<>/-=";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
